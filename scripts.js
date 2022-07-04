f329702f460393a8d4693359c4bfe8e16a1a68f0
Gabriel <vdvsa159946@gmail.com>
2022-07-04 10:18:29 -0300

    slide configurado

--- scripts.js
+++ scripts.js
@@ -9,35 +9,27 @@ new Glider(document.querySelector('.js-carousel--simple'), {
   }
 })
 
-new Glider(document.querySelector(".glider-produtos"), {
-  slidesToShow: 2.5,
-  slidesToScroll: 1,
-  scrollLock: true,
-  draggable: true,
-  arrows: {
-    prev: '.prev',
-    next: '.next',
-  },
-  settings: {
-    itemWidth: '100%',
-  },
-  responsive: [{
-    breakpoint: 1250,
-    settings: {
-      // Set to `auto` and provide item width to adjust to viewport
-      slidesToShow: 2.5,
-      slidesToScroll: 1,
-      itemWidth: 150,
-      duration: 0.25
-    },
 
-    breakpoint: 320,
-    settings: {
-      // Set to `auto` and provide item width to adjust to viewport
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      itemWidth: 150,
-      duration: 0.25
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10
     }
-  }],
-})
+  }
+
+});
