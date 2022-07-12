e5d417a37e1218213d74ed744bc809b61da45f74
Gabriel <vdvsa159946@gmail.com>
2022-07-12 20:07:37 -0300

    finalizando o fetch de post

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,22 +1,39 @@
 export default function postNewsletter() {
   
 }
-const form = document.querySelector('[action="newsletter"] #email').value;
-const submit = document.querySelector('.btn-newsletter');
-console.log(form)
 
-
-function fetchPostDatas() {
-  fetch('https://corebiz-test.herokuapp.com/api/v1/newsletter', {
-    method: 'POST',})
-  }
-  
-  
-  function postForm(event) {
-    event.preventDefault();
-    fetch(form).then(r => r.json()).then(json => console.log(json))
+const invalidClass = 'invalido';
+const elements = {
+  form: document.querySelector('[action="newsletter"]'),
+  inputs: document.querySelectorAll('.errorpost'),
+  btnSubmit: document.querySelector('.btn-newsletter'),
 }
 
-submit.addEventListener('click', postForm);
+function fetchPostDatas(element) {
+  fetch('https://corebiz-test.herokuapp.com/api/v1/newsletter', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(element)
+  }).then((r) => {
+    if (!r.ok) {
+      elements.form.classList.add(invalidClass);
+      elements.inputs.forEach(item => item.classList.add(invalidClass))
+    } else {
+      elements.form.classList.remove(invalidClass);
+      elements.inputs.forEach(item => item.classList.remove(invalidClass))
+    }
+  });
+  }
+  
+  function postForm(event) {
+  event.preventDefault();
+  const formObj = {
+  "email": `${elements.form.email.value}`,
+  "name": `${elements.form.name.value}`
+  }
+  fetchPostDatas(formObj);
+}
 
-// 'https://corebiz-test.herokuapp.com/api/v1/newsletter'
+elements.btnSubmit.addEventListener('click', postForm);
